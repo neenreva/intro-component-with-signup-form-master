2d9124ebd0c3e1f7377d7904960e34a9c3319262
Jimmy G <neenreva@gmail.com>
2022-03-04 12:12:36 -0500

    Error addition & Final submission

--- app.js
+++ app.js
@@ -1,43 +1,41 @@
-console.log("hello!");
+console.log("hello there!");
 
 const firstName = document.querySelector(".input-fn");
 const lastName = document.querySelector(".input-ln");
 const email = document.querySelector(".input-ea");
 const password = document.querySelector(".input-pw");
 const freeTrialBtn = document.querySelector(".free-trial");
-// console.log(firstName, lastName, email, password, freeTrialBtn);
 
 freeTrialBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  // console.log('btn');
-  emptyField(firstName)
-//   emptyField(lastName);
+  emptyField(firstName);
+  emptyField(lastName);
   emailValidation(email);
-//   emptyField(password);
+  emptyField(password);
 });
 
 function emptyField(field) {
-  console.log(field.parentElement.children);
-    if (field.parentElement.children.length > 1) { //remove error HTML
-    }
-  let error = `<span class="icon"
-    ><img src="images/icon-error.svg" alt="error icon"
-    /></span>
-    <span class="error">${field.placeholder} cannot be empty</span>`;
-
-  if (field.value == "") {
-    field.parentElement.insertAdjacentHTML("beforeend", error);
-  } 
-
+  console.log((field.style.border += "1px solid red"));
+  if (
+    field.value == "" &&
+    field.parentElement.children[1].classList.contains("hidden")
+  ) {
+    field.style.border = "1px solid red";
+    field.parentElement.children[1].classList.remove("hidden");
+  } else {
+    field.parentElement.children[1].classList.add("hidden");
+    field.style.border = "1px solid var(--grayishViolet)";
+  }
 }
 
 function emailValidation(field) {
-    let error = `<span class="icon"
-    ><img src="images/icon-error.svg" alt="error icon"
-    /></span>
-    <span class="error">Looks like this is not a valid ${field.placeholder}</span>`;
+  console.log(field.value);
 
-  if (field.value == "") {
-    field.parentElement.insertAdjacentHTML("beforeend", error);
+  if (!field.value.match(/^\S+@\S+\.\S+$/m)) {
+    field.parentElement.children[1].classList.remove("hidden");
+    field.style.border = "1px solid red";
+  } else {
+    field.parentElement.children[1].classList.add("hidden");
+    field.style.border = "1px solid var(--grayishViolet)";
   }
 }
